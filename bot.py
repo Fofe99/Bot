2dde97fa760f1310c05a1498811175ac2cb89441
Fofe99 <167451406+Fofe99@users.noreply.github.com>
2024-04-18 20:51:48 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,8 @@
 from OpsAi import Ai
 from pyrogram import Client,filters
 
-API_ID = 6341852765
-API_HASH = "03488b3c030fe095667e7ca22fe34954"
+API_ID = 23507256
+API_HASH = "7618aa57fcf8b9d81b5d5e668030d685"
 TOKEN = "6817446540:AAGeg55aXe6BRC2UzPQf3Qsrm56e2ZDLVag:"
 app = Client("ChatGpt", api_id=API_ID,api_hash=API_HASH,bot_token=TOKEN) 
 @app.on_message(filters.command("start"))
